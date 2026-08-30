0722163b56304dbe42cc72c211fe3d2963be4945
Prakhar Dixit <prakhardixit779@gmail.com>
2026-08-31 04:27:45 +0530

    This is all of my first Day practise

---
+++ Day1.py
@@ -0,0 +1,31 @@
+# print("Prakhar")
+# print("My age is 31")
+# name = input("Enter your name : ")
+# print(name)
+
+# name = "Prakhar"
+# city = "Bareilly"
+# age = 31
+# print(name)
+# print(city)
+# print(age)
+
+# new_name = input("Enter your name : ")
+# print("Hello " + new_name + "!")
+
+# age_1 = input("Enter your age : ")
+# print("Age is :" + age_1)
+# num1 = input("Enter first number : ")
+# num2 = input("Enter second number : ")
+# sum = int(num1) + int(num2)
+# print("Sum of numbers is :" + str(sum))
+
+print("STUDENT INFORMATION SYSTEM")
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+city = input("Enter your city : ")
+profession = input("Enter your profession : ")
+print("Name : " + name)
+print("Age :" + age)
+print("City :" + city)
+print("Profession :" + profession)
